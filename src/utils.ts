4236c6522b0b6d373497b939e23af67f1af2be7f
Bradley <bradleysuira@gmail.com>
2021-03-24 22:06:03 -0500

    fixed parsing for data buffers

--- src/utils.ts
+++ src/utils.ts
@@ -1,43 +1,43 @@
-export const parseInputBuffer = (input: any): any => {
-    if (input) {
-        if(input.data && input.type && input.type === 'Hex') {
-            return Buffer.from(input.data, 'hex');
+export const parseInputBuffer = (payload: any): any => {
+    if (payload) {
+        if(payload.type && payload.type === 'Hex') {
+            return Buffer.from(payload.data || '', 'hex');
         }
     
-        if(input instanceof Array) {
-            return input.map(i=> parseInputBuffer(i));
+        if(payload instanceof Array) {
+            return payload.map(i=> parseInputBuffer(i));
         }
     
-        if(typeof input === 'object' && Object.keys(input).length > 0) {
+        if(typeof payload === 'object' && Object.keys(payload).length > 0) {
             const output = {};
-            for (const key in input) {
-                output[key] = parseInputBuffer(input[key]);
+            for (const key in payload) {
+                output[key] = parseInputBuffer(payload[key]);
             }
             return output;
         }
     }
 
-    return input
+    return payload
 }
 
-export const parseOutputBuffer = (input: any): any => {
-    if(input instanceof Buffer) {
-        return { type: 'Hex', data: input.toString('hex') };
+export const parseOutputBuffer = (payload: any): any => {
+    if(payload instanceof Buffer) {
+        return { type: 'Hex', data: payload.toString('hex') };
     }
 
-    if(input instanceof Array) {
-        return input.map(i=> parseOutputBuffer(i));
+    if(payload instanceof Array) {
+        return payload.map(i=> parseOutputBuffer(i));
     }
 
-    if (input && typeof input === 'object') {
-        if(Object.keys(input).length > 0) {
+    if (payload && typeof payload === 'object') {
+        if(Object.keys(payload).length > 0) {
             const output = {};
-            for (const key in input) {
-                output[key] = parseOutputBuffer(input[key]);
+            for (const key in payload) {
+                output[key] = parseOutputBuffer(payload[key]);
             }
             return output;
         }
     }
 
-    return input
+    return payload
 }
