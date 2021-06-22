41ece59ce81b733930d8b531e533de0785c01766
Bradley <bradleysuira@gmail.com>
2021-06-22 09:18:49 -0500

    feat: added changes tu support webusb

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
 var global = global || window;
+declare var chrome: any;
 
 import './polyfills';
 
 import { LedgerWebBridge } from "./LedgerWebBridge";
 
 const run = () => {
-    const bridge = new LedgerWebBridge();
-    bridge.startListening();
-    console.log('LEDGER-BRIDGE: start listening')
+    const trigger = document.querySelector('#usb');
+    trigger.addEventListener('click', async (e) => {
+        try {
+            const device = await (navigator as any).usb.requestDevice({filters: [{
+                vendorId: 0x2c97
+            }]});
+            console.log('device:', device);
+            const bridge = new LedgerWebBridge();
+            bridge.startListening();
+            bridge.sendMessage(
+                {
+                    action: 'test',
+                    success: true,
+                    payload: { a: 1}
+                })
+            console.log('LEDGER-BRIDGE: start listening');
+        } catch (err) {
+            console.error(err);
+        }
+    });
 }
 
 run();
